3ceec07f2ba8e14022afa1bb02068c9aa8e58b50
Roman Kazachkov <roman.kazachkov1@gmail.com>
2025-10-26 18:31:05 +0300

    Fix 3

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,20 @@ fn main() {
 
     loop {
         let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
 
-        if io::stdin().read_line(&mut input).is_err() {
+        let num: i32 = input.trim().parse().unwrap();
+
+        if num == -1 {
+            println!("{}", sum);
+            return;
+        }
+
+        if num < 0 {
             println!("NaN");
             return;
         }
 
-        let input = input.trim();
-
-        if input.is_empty() {
-            continue;
-        }
-
-        match input.parse::<i32>() {
-            Ok(num) => {
-                if num == -1 {
-                    println!("{}", sum);
-                    return;
-                } else if num > 0 {
-                    sum += num;
-                } else {
-                    println!("NaN");
-                    return;
-                }
-            }
-            Err(_) => {
-                println!("NaN");
-                return;
-            }
-        }
+        sum += num;
     }
 }
